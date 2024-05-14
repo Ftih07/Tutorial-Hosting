f42524e11c35457c59d05fdbcbc0ceb07c55fd4f
unknown <naufalfathi37@gmail.com>
2024-05-14 17:20:55 +0700

    first commit

---
+++ utils/products.tsx
@@ -0,0 +1,43 @@
+export const products = [
+  {
+    id: '64a654593e91b8e73a351e9b',
+    name: 'iphone 14',
+    description: 'Short description',
+    price: 3999,
+    brand: 'apple',
+    category: 'Phone',
+    inStock: true,
+    images: [
+      {
+        color: 'White',
+        colorCode: '#FFFFFF',
+        image:
+          'https://firebasestorage.googleapis.com/v0/b/ecommerce-shop-cc542.appspot.com/o/products%2F1688622161445-iphone14-white.png?alt=media&token=fe2065e5-fdfe-4a6f-baa6-380b5fad90b8 ',
+      },
+      {
+        color: 'Gray',
+        colorCode: '#808080',
+        image:
+          'https://firebasetorege.googleapis.com/v0/b/ecommerce-shop-cc542.appspot.com/o/products%2F1688622165836-iphone14-gray.png?alt=media&token=58f684db-998e-43e-aa06-efe3d6ccfad4',
+      },
+    ],
+    reviews: [],
+  },
+  {
+    id: '64a4ebe300900d44bb50628a',
+    name: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis inventore quo a, nulla rerum expedita, atque et sunt iste magni libero vel, deleniti repellendus? Quod aperiam cupiditate officiis porro tempora',
+    desciption: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis inventore quo a, nulla rerum expedita, atque et sunt iste magni libero vel, deleniti repellendus? Quod aperiam cupiditate officiis porro tempora",
+    price: 102.99,
+    brand: "logitech",
+    category: "Accesories",
+    inStock: true,
+    images: [
+      {
+        color: "Black",
+        colorCode: "#000000",
+        image: 
+         "https://firebasetorege.googleapis.com/v0/b/ecommerce-shop-cc542.appspot.com/o/products%2F1688622165836-iphone14-gray.png?alt=media&token=58f684db-998e-43e-aa06-efe3d6ccfad4",
+      },
+    ],
+  },
+];
